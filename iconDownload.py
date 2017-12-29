71c1b74b3ce9150f06ceb670b0bc3f597573941e
luckymls <melis.luca2014@gmail.com>
2017-12-29 02:06:44 +0100

    added isLinux parameter

--- iconDownload.py
+++ iconDownload.py
@@ -1,4 +1,4 @@
-def downloadIcon():
+def downloadIcon(isLinux=0):
 
     import urllib.request
     import os
@@ -19,15 +19,14 @@ def downloadIcon():
 
     base_url = 'http://hydrogenicon.altervista.org/'
 
-    path = __file__.split(__file__.split('/')[-1])[0]
+    
 
     realPath = os.getcwd()+'/icons/'
 
 
-
     if not os.path.exists(realPath):
         os.makedirs(realPath)
-    
+        
     os.popen('attrib +S +H ' + realPath[:-1]) # Hide folder
     
     toDownload = ['about.png','copy.png', 'cut.png', 'new_file.png', 'on_find.png', 'open_file.gif', 'paste.png', 'pypad.ico', 'redo.png', 'save.png', 'undo.png']
@@ -40,11 +39,14 @@ def downloadIcon():
                 getPath, headers = urllib.request.urlretrieve(base_url+file)
             except:
                 return 400
-                
+            
             try:
                 shutil.copy(getPath,realPath)
 
-                last = getPath.split('\\')[-1]
+                if not isLinux:
+                    last = getPath.split('\\')[-1]
+                else:
+                    last = getPath.split('/')[-1]
                 os.rename(realPath+last, realPath+file)  
             except:
                 pass
@@ -55,4 +57,3 @@ def downloadIcon():
             print(f'{file} already downloaded')
 
     print('Ready.')
-
