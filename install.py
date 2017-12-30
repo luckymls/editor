2edefa3200ed530bc72b40ae17af920403bd09fb
luckymls <melis.luca2014@gmail.com>
2017-12-30 02:28:17 +0100

    Update install.py

--- install.py
+++ install.py
@@ -5,15 +5,15 @@ import sys
 if sys.platform[:5].lower() == 'linux':
 	isLinux = 1
 else:
-	isLinux = 0operating_sys = sys.platform
+	isLinux = 0
 	
 program_dir = os.getcwd()
 if not isLinux:
-	if not os.path.exists('C:\Program Files (x86)\hydrogen'):
-		os.makedirs('C:\Program Files (x86)\hydrogen')
-	shutil.copytree(f'{program_dir}', 'C:\Program Files (x86)\hydrogen')
-	os.system(f'pathman /au C:\Program Files (x86)\hydrogen')
+	if not os.path.exists('C:/\/Program Files (x86)/\/hydrogen'):
+		os.makedirs('C:/\/Program Files (x86)/\/hydrogen')
+	shutil.copytree(f'{program_dir}', 'C:/\/Program Files (x86)/\/hydrogen')
+	os.system(f'pathman /au C:/\/Program Files (x86)/\/hydrogen')
 
 else:
 	pass
-	# copy_tree(f'
+	
