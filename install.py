b1f42de6c2ac5a27286958de6cb13affff732eda
luckymls <melis.luca2014@gmail.com>
2017-12-30 03:17:10 +0100

    Update install.py

--- install.py
+++ install.py
@@ -6,14 +6,33 @@ if sys.platform[:5].lower() == 'linux':
 	isLinux = 1
 else:
 	isLinux = 0
-	
+
 program_dir = os.getcwd()
+
+def reAssemble(arg=None):
+    global isLinux
+    res = ''
+    for Dir in arg:
+        if not isLinux:
+            res += Dir+'\\'
+        else:
+            res += Dir+'/'
+    if isLinux:
+        return res+'.editorX_Z'
+    else:
+        return res+'editorX_Z'
+
+if isLinux:
+	path = reAssemble(program_dir.split('/')[:-1])
+else:
+	path = reAssemble(program_dir.split('\\')[:-1])
+    
+    
 if not isLinux:
-	if not os.path.exists('C:/\/Program Files (x86)/\/hydrogen'):
-		os.makedirs('C:/\/Program Files (x86)/\/hydrogen')
-	shutil.copytree(f'{program_dir}', 'C:/\/Program Files (x86)/\/hydrogen')
-	os.system(f'pathman /au C:/\/Program Files (x86)/\/hydrogen')
+    
+    shutil.copytree(f'{program_dir}', path)
+    os.popen('attrib +S +H ' + path)
+    os.system(f'pathman /au {path}')
 
 else:
-	pass
-	
+    pass
