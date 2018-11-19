1546e51372321b3f15831bcafa79fa9b211786e1
luckymls <melis.luca2014@gmail.com>
2018-11-19 23:50:00 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,26 @@
+# Librerie built-in
+
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter import filedialog
+from tkinter import messagebox
+import tkinter.font
+import os
+import sys
+import time
+import random
+import re
+import ast
+import json
+import pygments
+from pygments import lexers
+
+
+# Librerie personali
+
+from iconDownload import *  # Libreria per scaricare le icone
+from themeDownload import * # Libreria per scaricare i temi
+
+# Classi personali
+
+from class_config import *
